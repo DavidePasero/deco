e50676d8f1c8d11c00715f424a125cc151ce7f15
lukas <bierling.lukas@gmail.com>
2025-05-28 22:42:52 +0200

    added vlm text creator script

---
+++ create_vlm_texts.py
@@ -0,0 +1,13 @@
+from models.vlm import VLMManager
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_dir', type=str, required=True, help='path to image directory')
+    parser.add_argument('--out_dir', type=str, default="./cache/vlm_texts_cache", help='path to output directory')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
+    args = parser.parse_args()
+    vlm_manager = VLMManager()
+    imgs = [os.path.join(args.img_dir, x) for x in os.listdir(args.img_dir)]
+    vlm_manager.generate_texts(imgs, args.batch_size)
